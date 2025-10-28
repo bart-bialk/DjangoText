81c9b583f9aaf7a61734bac671e5ef1792937298
bart-bialk <bartbialk@gmail.com>
2025-10-28 14:49:42 +0100

    project skelet

---
+++ TextApp/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+
+import random
+from django.shortcuts import render
+from .forms import UploadFileForm
+
+def shuffle_word(word):
+    if len(word) <= 3:
+        return word
+    middle = list(word[1:-1])
+    random.shuffle(middle)
+    return word[0] + ''.join(middle) + word[-1]
+
+def shuffle_text(text):
+    words = text.split()
+    shuffled_words = [shuffle_word(word) for word in words]
+    return ' '.join(shuffled_words)
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            uploaded_file = request.FILES['file']
+            text = uploaded_file.read().decode('utf-8')
+            shuffled_text = shuffle_text(text)
+            return render(request, 'TextApp/result.html', {'text': shuffled_text})
+    else:
+        form = UploadFileForm()
+    return render(request, 'TextApp/upload.html', {'form': form})
+
